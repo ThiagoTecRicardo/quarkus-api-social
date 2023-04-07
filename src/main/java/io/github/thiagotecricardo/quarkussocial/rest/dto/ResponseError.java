1cf512aae017361e7765f3c8a89f5d9d2ee3fe45
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2023-04-07 20:45:32 -0300

    Retornando um objeto de erro padrão

--- src/main/java/io/github/thiagotecricardo/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/io/github/thiagotecricardo/quarkussocial/rest/dto/ResponseError.java
@@ -2,12 +2,15 @@ package io.github.thiagotecricardo.quarkussocial.rest.dto;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintViolation;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ResponseError {
+
+    public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
     private String message;
 
@@ -43,5 +46,9 @@ public class ResponseError {
 
     public void setErrors(Collection<FieldError> errors) {
         this.errors = errors;
+    }
+
+    public Response withStatusCode(int code){
+        return Response.status(code).entity(this).build();
     }
 }
