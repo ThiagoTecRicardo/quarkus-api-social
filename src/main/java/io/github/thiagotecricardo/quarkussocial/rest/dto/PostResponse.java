203117ae442f0b39e842721d5a42267c2a3d9c9b
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2023-04-08 13:45:03 -0300

    Concluindo a API de Postagens

---
+++ src/main/java/io/github/thiagotecricardo/quarkussocial/rest/dto/PostResponse.java
@@ -0,0 +1,22 @@
+package io.github.thiagotecricardo.quarkussocial.rest.dto;
+
+import io.github.thiagotecricardo.quarkussocial.domain.model.Post;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class PostResponse {
+
+    private String text;
+    private LocalDateTime dateTime;
+
+    public static PostResponse fromEntity(Post post){
+
+        var response = new PostResponse();
+        response.setText(post.getText());
+        response.setDateTime(post.getDateTime());
+        return response;
+
+    }
+}
